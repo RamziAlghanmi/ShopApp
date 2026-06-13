fee936bcd9a524513b97ac3ef70d63b0842f4fbd
RamziAlGhanmi <ramzi.alghanmi200411@gmail.com>
2026-06-13 22:39:49 +0300

    add abi

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,31 +1,4 @@
-// class Product {
-//   final String id;
-//   final String name;
-//   final String description;
-//   final double price;
-//   final String imageUrl;
-//   final String category;
 
-//   const Product({
-//     required this.id,
-//     required this.name,
-//     required this.description,
-//     required this.price,
-//     required this.imageUrl,
-//     required this.category,
-//   });
-
-//   factory Product.fromJson(Map<String, dynamic> json) {
-//     return Product(
-//       id: json['id']?.toString() ?? '',
-//       name: json['name'] ?? '',
-//       description: json['description'] ?? '',
-//       price: double.tryParse(json['price'].toString()) ?? 0.0,
-//       imageUrl: json['imageUrl'] ?? '',
-//       category: json['category'] ?? '',
-//     );
-//   }
-// }
 
 // From bummyjson abi
 class Product {
